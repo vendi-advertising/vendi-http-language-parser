89f805185ff90e78e4a29673ec746ef068795aff
cjhaas <chris@vendiadvertising.com>
2018-04-17 10:23:51 -0500

    PHP CS

--- tests/test-LanguageHelper.php
+++ tests/test-LanguageHelper.php
@@ -12,20 +12,18 @@ use Vendi\HttpLanguageHelper\LanguageHelper;
  */
 class test_LanguageHelper extends TestCase
 {
-
     /**
      * @covers \Vendi\HttpLanguageHelper\LanguageHelper::get_server_language_from_client_string
      */
     public function test_get_server_language_from_client_string()
     {
-        $this->assertSame( 'ch', LanguageHelper::get_server_language_from_client_string( 'ch', [ 'en', 'de' ], '' ) );
-        $this->assertSame( 'ch', LanguageHelper::get_server_language_from_client_string( 'ch', [ ], 'fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5' ) );
+        $this->assertSame('ch', LanguageHelper::get_server_language_from_client_string('ch', [ 'en', 'de' ], ''));
+        $this->assertSame('ch', LanguageHelper::get_server_language_from_client_string('ch', [ ], 'fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'));
 
-        $this->assertSame( 'fr-CH', LanguageHelper::get_server_language_from_client_string( 'ch', [ 'fr-CH', 'fr' ], 'en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH' ) );
-        $this->assertSame( 'fr', LanguageHelper::get_server_language_from_client_string( 'ch', [ 'en', 'fr' ], 'en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH' ) );
+        $this->assertSame('fr-CH', LanguageHelper::get_server_language_from_client_string('ch', [ 'fr-CH', 'fr' ], 'en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH'));
+        $this->assertSame('fr', LanguageHelper::get_server_language_from_client_string('ch', [ 'en', 'fr' ], 'en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH'));
 
 
-        $this->assertSame( 'ch', LanguageHelper::get_server_language_from_client_string( 'ch', [ 'gf', 'zi' ], 'en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH' ) );
-
+        $this->assertSame('ch', LanguageHelper::get_server_language_from_client_string('ch', [ 'gf', 'zi' ], 'en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH'));
     }
 }
